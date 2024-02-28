dcf10d1bb1192ee58cfb443a6123ba3345bea526
shelarumesh <shelarumesh1006@gmail.com>
2024-02-28 12:53:17 +0530

    project Structure

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,12 @@
 import sys
 import logging
+from src.logger import logging
 
 
 def error_message_details(error, error_detail:sys):
     _,_,exe_tb = error_detail.exc_info()
     file_name=exe_tb.tb_frame.f_code.co_filename
-    error_message = 'Error occured in python script name-{} line number [{}] error massage[{}]'.format(
+    error_message = 'Error occured in python script name-[{0}] line number [{1}] error massage[{2}]'.format(
         file_name, exe_tb.tb_lineno, str(error)
     )
 
@@ -19,3 +20,14 @@ class CustomException(Exception):
 
     def __str(self):
         return self.error_message
+
+def __str__(self):
+    return self.error_message
+
+
+if __name__ == '__main__':
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Divide by zero')
+        raise CustomException(e, sys)
